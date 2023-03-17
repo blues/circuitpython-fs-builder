438efb48f3be8c9daf23bd41f4edbce485bd719d
Matthew McGowan <mat.mcgowan+git@gmail.com>
2023-03-17 12:06:48 -0700

    feat: adds setup.py so that launcher scripts are added to the Python environment. Installation follows the usual `pip install .` for installing from sources.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+   name='cpy-fs',
+   version='1.0',
+   description='Create FAT filesystem images for use with CircuitPython.',
+   author='Matthew McGowan',
+   author_email='mat@blues.com',
+   install_requires=['wheel',
+                     'appdirs>=1.4.4',
+                     'fs>=2.4.15',
+                     'pyfatfs>=1.0.5'
+                     ],
+   packages=['cpyfs'],
+   entry_points={
+       'console_scripts': [
+         'cpy-fs = cpyfs.makefs:main',
+         'cpy-fs-dir = cpyfs.tree:main',
+      ],
+   },
+)
